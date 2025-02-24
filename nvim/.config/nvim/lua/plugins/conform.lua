48eff6e557f3de6afca5675a5a42f268af24b163
Matthew Wagerfield <matthew@wagerfield.com>
2025-02-24 11:43:17 +0000

    feat: configure conform.nvim to use prettier

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+    },
+  },
+}
