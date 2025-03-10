6446a802d22767392cafa8cd9facdb5cbd9eb132
Matthew Wagerfield <matthew@wagerfield.com>
2025-03-10 21:01:11 +0000

    feat: add bufferline plugin config

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  enabled = false,
+  opts = {
+    options = {
+      close_icon = "✗",
+      buffer_close_icon = "✗",
+      max_name_length = 16,
+      tab_size = 16,
+    },
+  },
+}
