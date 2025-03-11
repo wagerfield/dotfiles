2c82d066680fe7a4f475105ba81f32091a169765
Matthew Wagerfield <matthew@wagerfield.com>
2025-03-11 21:01:49 +0000

    feat: configure snacks plugin

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,11 +1,28 @@
+require("snacks")
+
+local exclude = { ".git", ".DS_Store", "node_modules" }
+
 return {
   "folke/snacks.nvim",
+  ---@type snacks.Config
   opts = {
     picker = {
       formatters = {
         file = {
           filename_first = true,
           truncate = 80,
+        },
+      },
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          exclude = exclude,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+          exclude = exclude,
         },
       },
     },
