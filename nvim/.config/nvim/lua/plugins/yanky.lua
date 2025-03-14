7d889240eabe0196f1990505cd1c2e480e29face
Matthew Wagerfield <matthew@wagerfield.com>
2025-03-14 15:12:00 +0000

    feat: refine yanky configuration

--- nvim/.config/nvim/lua/plugins/yanky.lua
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -1,12 +1,18 @@
 return {
   -- https://github.com/gbprod/yanky.nvim#readme
   "gbprod/yanky.nvim",
-  opts = {},
+  opts = {
+    highlight = {
+      on_yank = true,
+      on_put = true,
+      timer = 250,
+    },
+    textobj = {
+      enabled = true,
+    },
+  },
   keys = {
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-
-    { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next yank entry" },
-    { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous yank entry" },
 
     { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
     { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
@@ -15,16 +21,19 @@ return {
     { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
 
     { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
     { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
     { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
 
-    { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
-    { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+    { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put after and indent left" },
+    { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put after and indent right" },
     { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
     { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
 
     { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
     { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
+
+    { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous yank entry" },
+    { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next yank entry" },
   },
 }
