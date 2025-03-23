2483c71ade5a169e0591bb187ab5c169f35ea1a5
Matthew Wagerfield <matthew@wagerfield.com>
2025-03-23 23:31:45 +0000

    feat: update yazi keymap

--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -1,11 +1,14 @@
+require("which-key").add({
+  { "<leader>y", group = "yazi", icon = "󱞊 " },
+})
+
 return {
   -- https://github.com/mikavilpas/yazi.nvim#readme
   "mikavilpas/yazi.nvim",
-  dependencies = { "folke/snacks.nvim", lazy = true },
   event = "VeryLazy",
   keys = {
-    { "<leader>yy", "<cmd>Yazi toggle<cr>", desc = "Toggle yazi" },
-    { "<leader>yd", "<cmd>Yazi cwd<cr>", desc = "Open yazi in cwd" },
     { "<leader>yf", "<cmd>Yazi<cr>", desc = "Open yazi at file", mode = { "n", "v" } },
+    { "<leader>yr", "<cmd>Yazi cwd<cr>", desc = "Open yazi at root" },
+    { "<leader>yy", "<cmd>Yazi toggle<cr>", desc = "Toggle yazi" },
   },
 }
