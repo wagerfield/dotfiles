98456fda4a36db04f1a8e18935ea91c56ba4a992
Matthew Wagerfield <matthew@wagerfield.com>
2025-04-08 22:30:42 +0100

    feat: add conditional neovim configuration for vscode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,7 @@
-require("config.lazy")
-require("config.commands")
+if vim.g.vscode then
+  vim.keymap.set("n", "<c-u>", "<c-u>zz", { desc = "Move Screen Up" })
+  vim.keymap.set("n", "<c-d>", "<c-d>zz", { desc = "Move Screen Down" })
+else
+  require("config.lazy")
+  require("config.commands")
+end
