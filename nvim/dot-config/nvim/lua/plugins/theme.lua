a1a330fa8546abc391b68bd7aa99e14f2f9a765d
Matthew Wagerfield <matthew@wagerfield.com>
2026-04-30 23:17:06 +0100

    feat: add knangawa theme

--- nvim/dot-config/nvim/lua/plugins/theme.lua
+++ nvim/dot-config/nvim/lua/plugins/theme.lua
@@ -5,8 +5,30 @@ return {
     name = "kanagawa",
     lazy = false,
     priority = 1000,
-    config = function()
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
       vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+  {
+    -- https://github.com/neanias/everforest-nvim#readme
+    "neanias/everforest-nvim",
+    name = "everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme("everforest")
     end,
   },
   {
